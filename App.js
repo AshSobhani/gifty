977ef60189037fe73afc0e0a3814e11a670fbb1e
AshSobhani <ashsobhani@hotmail.com>
2020-12-08 17:51:49 +0000

    first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+import WishListScreen from './app/screens/WishListScreen';
+
+function app() {
+    return <WishListScreen />;
+}
+
+const styles = StyleSheet.create({});
+
+export default app;
---
+++ app/screens/WishListScreen.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+import Screen from '../components/Screen';
+import defaultStyles from '../config/defaultStyles';
+
+function WishListScreen(props) {
+    return (
+        <Screen style={styles.container}>
+            <View style={styles.textContainer}>
+                <Text style={defaultStyles.text}>Hello adam!</Text>
+            </View>
+            <View style={styles.textContainer}>
+                <Text style={defaultStyles.text}>Hello adam!</Text>
+            </View>
+            <View style={styles.textContainer}>
+                <Text style={defaultStyles.text}>Hello adam!</Text>
+            </View>
+        </Screen>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    textContainer: {
+        fontSize: 10,
+        padding: 25,
+        margin: 10,
+        backgroundColor: defaultStyles.colors.primary,
+        justifyContent: 'center',
+        borderRadius: 50,
+        alignItems: 'center',
+    },
+});
+
+export default WishListScreen;
